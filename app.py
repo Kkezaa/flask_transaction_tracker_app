8704272144a4c9675b387a0c2eca8a00baaf3d08
Kkezaa <kristijanknezevic389@gmail.com>
2025-11-25 16:37:46 +0100

    Added a search and total blance feature to the app

--- app.py
+++ app.py
@@ -82,6 +82,42 @@ def delete_transaction(transaction_id):
     return redirect(url_for("get_transactions")) #redirect the user to the transactions page.
         
 
+# Add a search feature 
+
+@app.route("/search", methods = ["GET", "POST"])
+def search_transactions():
+    if request.method == "POST":                       # get the minimum and maximum amount.
+        min_amount = request.form.get("min_amount")
+        max_amount = request.form.get("max_amount")
+
+        min_amount = float(min_amount)                # convert the variables into floats.
+        max_amount = float(max_amount)
+
+        filtered_transactions = [                    # use a list comprehension to filter out the min and max amount criteria.
+        
+            transaction for transaction in transactions if min_amount <= transaction["amount"] <= max_amount
+
+            ]
+
+    
+        return render_template("transactions.html", transactions = filtered_transactions ) #render the transactions.html, showing the filtered transactions.
+
+    return render_template("search.html") # if request is GET, render the search.html file so the user can enter the amounts to filter.
+
+
+@app.route("/balance")
+def total_balance():
+
+    amount_list = [] # made a empty list to store all of the transaction amounts
+
+    for transaction in transactions:               #itereting trough the transactions and adding the amoun values to the list
+        amount_list.append(transaction["amount"])
+
+    total = sum(amount_list)      # summing the list
+    
+    return f"Total balance : {total}"  # returning a simple display text (may improve it later)
+
+
 
 # Run the Flask app
     
